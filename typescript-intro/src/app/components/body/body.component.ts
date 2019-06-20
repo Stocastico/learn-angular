87e9eb51218a1c407010176df9abbf636b2ae75e
Stefano <stocastico@gmail.com>
2019-06-20 21:13:49 +0200

    Complete body logic

--- typescript-intro/src/app/components/body/body.component.ts
+++ typescript-intro/src/app/components/body/body.component.ts
@@ -7,7 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BodyComponent implements OnInit {
 
-  constructor() { }
+  animals:string[]
+  length: string
+  show=true;
+
+  constructor() { 
+    this.animals = [];
+  }
+
+  addAnimal(animal:any) {
+    if (animal.value.length > 0) {
+      console.log(animal.value)
+      this.animals.push(animal.value);
+      animal.value="";
+      this.getLastLength();
+      animal.focus();
+    }
+    return false;
+  }
+
+  getLastLength()  {
+    var len = this.animals[this.animals.length-1].length
+    this.length = "The length of this animal is " + len;
+  }
+
+  calcTotalLength()  {
+    console.log("calcTotalLength called")
+    var i:number; 
+    var total = 0; 
+
+    for(i = 0; i<this.animals.length; i++) {
+      total += this.animals[i].length;
+    }
+    this.length = "The total length is " + total;
+  }
 
   ngOnInit() {
   }
