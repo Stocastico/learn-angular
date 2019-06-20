756b7a2832d752df4123c8ccf786629b8138eb89
Stefano <stocastico@gmail.com>
2019-06-20 19:59:12 +0200

    Add body and footer components

--- typescript-intro/src/app/app.module.ts
+++ typescript-intro/src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.components'
+import { HeaderComponent } from './components/header/header.components';
+import { FooterComponent } from './components/footer/footer.component';
+import { BodyComponent } from './components/body/body.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
