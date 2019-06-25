9b363c28e738bc9407a67c98891c0b85364583df
Stefano <stocastico@gmail.com>
2019-06-25 21:43:27 +0200

    Specify todo to complete exercise

--- third-project/src/app/servicios/services.service.ts
+++ third-project/src/app/servicios/services.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core'
 @Injectable()
 export class ServicesService {
 
+    services: Servi[]
     header: String
     title: String
     desc: String
@@ -13,4 +14,21 @@ export class ServicesService {
         this.title = t
         this.desc = d
     }
+
+    getServices() {
+        //TODO
+    }
 }
+
+export interface Servi {
+    header: String
+    title: String
+    desc: String
+}
+
+// TODO
+// define interface con la clase servicio
+// in services service, define an array of servicio y crealos allí
+// en services.component no creamos la lista allí, sino llamamos el metodo
+// getServicios de aquí que devuelve 
+// la lista de servicios qe hemos definido aquí
