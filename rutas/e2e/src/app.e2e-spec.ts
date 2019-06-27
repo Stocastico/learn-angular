98ab8bac8a99785faeac883e3f0c46dc89f7527b
Stefano <stocastico@gmail.com>
2019-06-27 19:49:58 +0200

    Made a mess

--- rutas/e2e/src/app.e2e-spec.ts
+++ rutas/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to parameters!');
+    expect(page.getTitleText()).toEqual('Welcome to rutas!');
   });
 
   afterEach(async () => {
--- rutas/src/app/app.component.spec.ts
+++ rutas/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'parameters'`, () => {
+  it(`should have as title 'rutas'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('parameters');
+    expect(app.title).toEqual('rutas');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to parameters!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to rutas!');
   });
 });
